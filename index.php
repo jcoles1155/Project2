83e22011e2d4db193a6a7b33c13485822ecc7eb4
JC Coles <jcoles1155@gmail.com>
2014-10-03 00:07:58 -0700

    added HTML elements and logic as well as inline styles

--- index.php
+++ index.php
@@ -11,22 +11,69 @@ ini_set('display_errors', 1);
 
 	<meta charset="utf-8"> 
 
-	<title>Memory</title>
-	
-	
-
+	<title>Password Generator</title>
 	
 	<?php require 'logic.php'; ?>
+	
+	<style>
+		.container {
+			text-align:center;
+		}
+		
+		.password {
+			text-align:center;
+			font-size:3rem;
+			margin-bottom:25px;
+			color:#f39c12;
+			font-family:consolas,courier;
+			background-color:#eee;
+			display:inline-block;
+			padding:15px;
+			font-weight:800;
+		}
+		
+		#number_of_words {
+			width:40px;
+		}
+				
+		.details {
+			margin-top:25px;
+		}
+	</style>
+		
 	
 </head>
 <body> 
 
-	<?php foreach($Passwords as $key => $value): ?>
+		
+		
+	<div class='container'>
+		<h1>Password Generator</h1>
 	
-		<?php echo $key ?> is a <?php echo $value ?> <br>
-	
-	<?php endforeach; ?>
-	
+		<p class='password'>
+					</p>
+		
+		<form>
+			<p class='options'>
+			
+				<label for='number_of_words'># of Words</label>
+				<input maxlength=1 type='text' name='number_of_words' id='number_of_words' value=''>  (Max 9)
+				<br>
+					
+				<input type='checkbox' name='add_number' id='add_number' > 
+				<label for='add_number'>Add a number</label>
+				<br>
+				<input type='checkbox' name='add_symbol' id='add_symbol' > 
+				<label for='add_symbol'>Add a symbol</label>
+			</p>
+		
+			<input type='submit' class='btn btn-default' value='Another Password'>
+					
+		</form>
+		
+		
+			
+	</div>
 	
 	
 	
--- logic.php
+++ logic.php
@@ -21,4 +21,116 @@
        18   => 'turkey',
        19   => 'clock',
        20   => 'computer',
+           
     );
+    
+    
+    	$Password = "";
+    	$random_number = rand(0,count($Passwords));
+    	
+    	
+    	$count = 0;
+    	foreach($Passwords as $key => $value) {
+    		
+    		if($count == $random_number) {
+    			$Passwords[$value] = $Password; 
+    		}
+    	
+    		$count++;
+    	
+    	}
+	
+		
+		foreach( $_POST as $key => $value )  {
+		
+			if ($value == 1) {
+				$word1 = $Passwords[$random_number];
+				$word1 = $Password;
+			}
+			
+			else if ($value == 2) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word1 + $word2 = $Password;
+			}
+			
+			else if ($value == 3) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word1 + $word2 + $word3 = $Password;
+			}
+			
+			else if ($value == 4) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 = $Password;
+			}
+			
+			else if ($value == 5) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word5 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 + $word5 = $Password;
+			}
+			
+			else if ($value == 6) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word5 = array_rand($Passwords);
+				$word6 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 + $word5 + $word6 = $Password;
+			}
+			
+			else if ($value == 7) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word5 = array_rand($Passwords);
+				$word6 = array_rand($Passwords);
+				$word7 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 + $word5 + $word6 + $word7 = $Password;
+			}
+			
+			else if ($value == 8) {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word5 = array_rand($Passwords);
+				$word6 = array_rand($Passwords);
+				$word7 = array_rand($Passwords);
+				$word8 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 + $word5 + $word6 + $word7 + $word8 = $Password;
+			}
+			
+			else {
+				$word1 = array_rand($Passwords);
+				$word2 = array_rand($Passwords);
+				$word3 = array_rand($Passwords);
+				$word4 = array_rand($Passwords);
+				$word5 = array_rand($Passwords);
+				$word6 = array_rand($Passwords);
+				$word7 = array_rand($Passwords);
+				$word8 = array_rand($Passwords);
+				$word9 = array_rand($Passwords);
+				$word1 + $word2 + $word3 + $word4 + $word5 + $word6 + $word7 + $word8 + $word9 = $Password;
+			}
+			
+			echo $random_number;
+			echo $Password;
+			
+		
+		}
+
+	
+
+     
+?>
